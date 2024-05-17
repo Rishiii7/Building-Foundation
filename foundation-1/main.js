e3c765f5b715e96dcd68055d857f77d4d11d8bb8
= <7rishi20ss@gmail.com>
2024-05-16 22:21:15 -0600

    added get post method

--- foundation-1/main.js
+++ foundation-1/main.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+const jsonParser = bodyParser.json();
 
 const port = 3000
+
+app.use(jsonParser);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
+    console.log("request has reached");
+    for(let i=0; i<100; i++){}
     res.send("Hello World !");
 });
+
+app.post('/conversation', (req, res) => {
+    console.log("request has reached");
+    console.log(req.body);
+    res.send({
+        msg : "2+2=4"
+    });
+})
