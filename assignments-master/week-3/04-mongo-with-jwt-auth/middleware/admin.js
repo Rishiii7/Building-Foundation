b5af47fb0ebb065cbd45b8adbe7067fd74b414b8
= <7rishi20ss@gmail.com>
2024-05-19 19:30:01 -0600

    added middleware for admin users

--- assignments-master/week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ assignments-master/week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,26 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).send("Unauthorized");
+    }
+    try{
+        // remove Bearer from token
+        const tokenWithoutBearer = token.split(" ")[1];
+        // validate token
+        const response = jwt.verify(tokenWithoutBearer, JWT_SECRET);
+        console.log("user validated");
+        next();
+    }catch(err){
+        return res.status(401).json({
+            message: err.message
+        });
+    }
 }
 
 module.exports = adminMiddleware;
