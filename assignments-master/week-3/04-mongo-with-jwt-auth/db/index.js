f0f9e136cd3ba332ba06d09114ee2f8e7fcc2f5e
= <7rishi20ss@gmail.com>
2024-05-19 19:29:28 -0600

    Added schema

--- assignments-master/week-3/04-mongo-with-jwt-auth/db/index.js
+++ assignments-master/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://riso2414:QAZwsx%401234@cluster0.ud80h5k.mongodb.net/course_selling_app_jwt');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title : String,
+    description : String,
+    price : Number,
+    imageLink : String,
+    published : Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
