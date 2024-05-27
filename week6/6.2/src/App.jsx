146df81ee501c10404270ad3ee58a76a654e3b84
= <7rishi20ss@gmail.com>
2024-05-27 16:08:46 -0600

    useMemo

--- week6/6.2/src/App.jsx
+++ week6/6.2/src/App.jsx
@@ -1,75 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// // // // // // // // // // // // // // // // // // // // 
+// Create an app that does two things -                  //
+//    1. Increase the counter by 1                       //
+//    2. Let's user put a value in an input box(n)       //
+//       and you need to show from 1-n                   //
+// // // // // // // // // // // // // // // // // // // // 
 function App() {
-  // const [todos, setTodos] = useState([]);
-  
-  // useEffect(() => {
-  //   axios.get('https://sum-server.100xdevs.com/todos')
-  //     .then((res) => {
-  //       setTodos(res.data.todos);
-  //     });
-  // }, []);
-  const [id, setId] = useState(1);
-  const idArr = [1,2,3,4,5]
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
 
-  function buttonHandler(id) {
-    setId(id);
+  const inputHandler = (e) => {
+    const n = parseInt(e.target.value, 10) || 0;
+    // conle.log(typeof n);
+    setInputValue(n);
   }
 
+  const sum = useMemo( () => {
+    const n = inputValue;
+    return (n * (n+1)) / 2;
+  }, [inputValue]);
+
+  // function inputHandler(e) {
+  //   const n = parseInt (e.target.value);
+  //   // conle.log(typeof n);
+
+  //   const sumOfN =  ( son * (n + 1) ) / 2;
+  //   setInputValue(sumOfN);
+  // }
+
   return (
     <>
-      {
-        idArr.map( (id) => {
-          return ( 
-          <button onClick={ () => buttonHandler(id)} key={id}>
-            {id}
-          </button> 
-          )
-        } )
-      }
-      <div className='todo-list'>
-        {/* {
-          todos.map((todo) => (
-            <TodoGetById id={todo.id} key={todo.id} />
-          ))
-        } */}
-        <TodoGetById id={id}></TodoGetById>
+      <button onClick={() => {setCounter(counter+1)}}>Counter {counter}</button>
+      <br></br><br></br>
+      <input type="number"  placeholder='number' onChange={inputHandler}/>
+      <br></br>
+      <div className='sum-of-n'>
+          <h3><span>Sum is {sum}</span></h3>  
       </div>
+      
+      
     </>
-  );
-}
-
-function TodoGetById({ id}) {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
-      .then((res) => {
-        setTodo(res.data.todo);
-      });
-  }, [id]);
-
-  return (
-    <div key={id} className='each-todo'>
-      <Todo
-        title={todo.title}
-        description={todo.description}
-        completed={todo.completed}
-      />
-    </div>
-  );
-}
-
-function Todo({ title, description, completed}) {
-  return (
-    <>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <button>{completed ? 'Done' : 'Mark as Done'}</button>
-    </>
-  );
+  )
 }
 
 export default App;
