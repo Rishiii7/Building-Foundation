8f49dfd363cb15a912ed30818e36fb67b5611d09
= <7rishi20ss@gmail.com>
2024-05-31 09:00:03 -0600

    7.1

---
+++ week7/7.1/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+export default function Dashboard() {
+    return (
+        <>
+            <br />
+            <div className="container">
+                Dashboard Page
+            </div>
+        </>
+        
+    )
+}
