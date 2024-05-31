ea5f63ac74cdef0c2cbd39237d24675e5030b513
= <7rishi20ss@gmail.com>
2024-05-31 16:38:51 -0600

    asynchronus data queries intro

--- week7/7.2/src/App.jsx
+++ week7/7.2/src/App.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from 'react'
 import './App.css'
-import { jobsAtom, messagingAtom, networkAtom, notificationAtom, totalCountSelector } from './atoms'
-import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { linkedlnAtom, totalLinkedlnSelector } from './atoms'
+import { RecoilRoot, useRecoilValue, useSetRecoilState, useRecoilState } from 'recoil'
+import axios from 'axios'
 
 
 
@@ -20,13 +22,33 @@ function App() {
 
 const LinkedlnBar = () =>{
 
-  const newtorkCount = useRecoilValue(networkAtom);
-  const jobCount = useRecoilValue(jobsAtom);
-  const messageCount = useRecoilValue(messagingAtom);
-  const notificationCount = useRecoilValue(notificationAtom);
+  // const [notificationCount, setNotification] = useRecoilState(notificationAtom);
+  // const [jobCount, setJob] = useRecoilState(jobsAtom);
+  // const [messageCount, setMessage] = useRecoilState(messagingAtom);
+  // const [newtorkCount, setNetwork] = useRecoilState(networkAtom);
 
-  const setMessageCount = useSetRecoilState(messagingAtom);
+  const [linkedlnCount, setLinkedln] = useRecoilState(linkedlnAtom);
 
+  // useEffect(()=>{
+  //   // console.log('useEffect')
+  //   axios.get('https://sum-server.100xdevs.com/notifications')
+  //   .then(  (res) => {
+  //     console.log(res)
+  //     let linkedlnCountBar = res.data;
+  //     console.log(linkedlnCountBar);
+      
+
+  //     // update the Atom
+  //     // setNetwork( c => linkedlnCountBar.network)
+  //     // setMessage( c => linkedlnCountBar.messaging)
+  //     // setJob( c => linkedlnCountBar.jobs)
+  //     // setNotification( c => linkedlnCountBar.notifications)
+  //     setLinkedln(c => c = linkedlnCountBar);
+
+  //   })
+  // }, []);
+
+  // console.log(linkedlnCount);
 
   return (
     <>
@@ -34,16 +56,20 @@ const LinkedlnBar = () =>{
       <button>Home</button>
 
       <button>
-        My Network ({newtorkCount >=100 ? '99+' : newtorkCount.toString()})
+        My Network ({linkedlnCount.network >=100 ? '99+' : linkedlnCount.network})
       </button>
       <button>
-        Jobs ({jobCount >=100 ? '99+' : jobCount.toString()})
+        Jobs ({linkedlnCount.jobs >=100 ? '99+' : linkedlnCount.jobs})
       </button>
-      <button onClick={() => {setMessageCount(c => c+1)}}>
-        Messaging ({messageCount >=100 ? '99+' : messageCount.toString()})
+      <button onClick={() => {setLinkedln( (c) => ({
+          ...c,
+          messaging : c.messaging + 1
+          })
+        )}}>
+        Messaging ({linkedlnCount.messaging >=100 ? '99+' : linkedlnCount.messaging})
       </button>
       <button>
-        Notification ({notificationCount >=100 ? '99+' : notificationCount.toString()})
+        Notification ({linkedlnCount.notifications >=100 ? '99+' : linkedlnCount.notifications})
       </button>
 
       <ButtonCount />
@@ -57,7 +83,7 @@ const LinkedlnBar = () =>{
 
 const ButtonCount = () => {
 
-  const totalCount =  useRecoilValue(totalCountSelector);
+  const totalCount =  useRecoilValue(totalLinkedlnSelector);
   return (
     <>
       <button>Me ({totalCount > 100 ? '99+' : totalCount})</button>
